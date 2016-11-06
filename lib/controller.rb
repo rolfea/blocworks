526d88441493736ec72f7b179cd29531b2e9289e
Alex Rolfe <rolfepercussion@gmail.com>
2016-11-06 17:17:05 -0600

    Adds support for instance variables in controller.rb

--- lib/controller.rb
+++ lib/controller.rb
@@ -10,6 +10,13 @@ module BlocWorks
       filename = File.join("app", "views", controller_dir, "#{view}.html.erb")
       template = File.read(filename)
       eruby = Erubis::Eruby.new(template)
+      # get instance variables from controller
+      self.instance_variables.each do |inst_var|
+        inst_var_value = self.instance_variable_get(inst_var)
+        # set them as eruby instance variables
+        eruby.instance_variable_set(inst_var, inst_var_value)
+      end
+      puts "THIS IS ERUBY: #{eruby.instance_variables}"
       eruby.result(locals.merge(env: @env))
     end
 
